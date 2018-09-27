7a3f45abf80892b0f4955669d5609794aef51617
setzer22 <jsanchezfsms@gmail.com>
2018-09-27 17:18:45 +0200

    Added backend folder and re-structured

---
+++ backend/project.clj
@@ -0,0 +1,13 @@
+(defproject backend "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [compojure "1.6.1"]
+                 [ring/ring-defaults "0.3.2"]
+                 [org.clojure/data.json "0.2.6"]]
+  :plugins [[lein-ring "0.12.4"]]
+  :ring {:handler backend.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]
+                        [ring/ring-mock "0.3.2"]]}})

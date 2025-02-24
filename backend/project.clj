314e0a664f99ec5c690568fa40c65aaf7e1cb900
jsfpadsupc <jsfpadsupc@proton.me>
2025-02-24 12:16:53 +0100

    ???

--- backend/project.clj
+++ backend/project.clj
@@ -5,6 +5,7 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
+                 [ring/ring-jetty-adapter "1.6.3"]
                  [com.cemerick/friend "0.2.3"]
                  [org.clojure/java.jdbc "0.7.8"]
                  [mysql/mysql-connector-java "8.0.12"]
@@ -12,6 +13,7 @@
   :plugins [[lein-ring "0.12.4"]]
   :ring {:handler backend.handler/app}
   :jvm-opts ["-Duser.timezone=UTC"]
+  :main backend.handler
   :profiles
   {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]
                         [ring/ring-mock "0.3.2"]]}})
